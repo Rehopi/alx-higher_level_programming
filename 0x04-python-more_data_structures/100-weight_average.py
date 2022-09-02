3da7b1aa4cdff12aa9bdf7c290ad3d95c25fc1b3
rehopi <emekaokechukwu16@gmail.com>
2022-09-02 12:47:54 -0700

    function that returns the w
    eighted average of all integers tuple (<score>, <weight>)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-
-    return sum(map(lambda x: x[0] * x[1], my_list)) / \
-            sum(map(lambda x: x[1], my_list))
+    if my_list:
+        total = 0
+        divisor = 0
+        for x in my_list:
+            total += x[0] * x[1]
+            divisor += x[1]
+        return total/divisor
+    return 0
