a12b5a7d7852cbf5fe66048cf3ba4a33f391fccb
rehopi <emekaokechukwu16@gamil.com>
2022-10-03 21:09:19 -0700

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module creates the Rectangle class that inherits from Base"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A class named Rectangle
+
+    Attributes:
+    attr1(id): id of object
+    attr2(width): rectangle width
+    attr3(height): rectangle height
+    attr4(x): number of spaces before rectangle
+    attr5(y): number of newlines before rectangle
+    """
