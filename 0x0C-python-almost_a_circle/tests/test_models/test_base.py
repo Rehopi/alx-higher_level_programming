5cfe20197ac8277c27250ff6cb73a031d5bc04e3
rehopi <emekaokechukwu16@gamil.com>
2022-10-03 19:48:38 -0700

    Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Tests Base Rectangle Square classes """
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
