aabd3b6e34d24fef21ca528e1473d3f06796df6a
rehopi <emekaokechukwu16@gmail.com>
2022-11-02 15:11:17 -0700

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,10 @@
---  all shows contained in hbtn_0d_tvshows with number of shows linked to each
--- lists all shows contained in hbtn_0d_tvshows with number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+-- all genres of the show Dexter
+-- lists all genres of the show Dexter
+SELECT tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
