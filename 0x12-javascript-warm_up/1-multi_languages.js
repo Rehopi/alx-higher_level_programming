0c28327d96b098491ab52b178dc079cc7ecfc415
rehopi <emekaokechukwu16@gmail.com>
2022-11-29 14:25:37 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-function main () {
-  let myVar = 'C is fun';
-  console.log(myVar);
-  myVar = 'Phyton is cool';
-  console.log(myVar);
-  myVar = 'JavaScript is amazing';
-  console.log(myVar);
-}
-main();
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
