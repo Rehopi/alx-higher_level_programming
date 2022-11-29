1032d00801992a87da4265b00d888a5e8ab0db7c
rehopi <emekaokechukwu16@gmail.com>
2022-11-29 15:51:24 -0800

    Ilove C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2], 10))) {
-  console.log('Missing number of occurences');
-} else {
-  let i;
-  for (i = 0; i < process.argv[2]; i++) { 
-    console.log('C is fun');
-  }
-}
+
+const argv1 = parseInt(process.argv[2]);
+
+if (isNaN(argv1)) console.log('Missing number of occurrences');
+else for (let i = 0; i < argv1; i++) console.log('C is fun');
