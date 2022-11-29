9f1edc2950757059540cc629a8331561b9e43c89
rehopi <emekaokechukwu16@gmail.com>
2022-11-29 15:08:56 -0800

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
+
 const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i;
-for (i = 0; i < strings.length; i++) {
-  console.log(strings[i]);
-}
+
+myStr.forEach((item) => console.log(item));
