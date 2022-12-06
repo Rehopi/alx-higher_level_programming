8b5d6549c48f439730db6f85eff654292f0a0bed
rehopi <emekaokechukwu16@gmail.com>
2022-12-06 04:09:05 -0800

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""This module contains a script that lists all cities of a passed state"""
+"""
+First state model
+"""
 
 from sys import argv
 import MySQLdb
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities LEFT OUTER JOIN states" +
-                " ON cities.state_id=states.id" +
-                " WHERE states.name='{}'".format(sys.argv[4]))
-    query_rows = cur.fetchall()
-    print(", ".join([row[0] for row in query_rows]))
-    cur.close()
-    conn.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    state = argv[4]
+    cur = db.cursor()
+    sql = """SELECT cities.name
+          FROM cities JOIN states
+          ON cities.state_id = states.id
+          WHERE states.name = %s ORDER by cities.id ASC"""
+          params = (state,)
+          cur.execute(sql, params)
+          query_rows = cur.fetchall()
+
+          print(", ".join(row[0] for row in query_rows))
+          cur.close()
+          db.close()
