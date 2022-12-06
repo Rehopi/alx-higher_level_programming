1e64cfc6d4467decdc8454e6c07ba5643a22b724
rehopi <emekaokechukwu16@gmail.com>
2022-12-06 04:00:02 -0800

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """This module contains a script that lists all cities of a passed state"""
 
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    query = "SELECT cities.name FROM cities"
-    query = query + " INNER JOIN states ON cities.state_id = states.id"
-    query = query + " WHERE states.name = %s ORDER BY cities.id ASC"
-    cur.execute(query, [sys.argv[4]])
+    cur.execute("SELECT cities.name FROM cities LEFT OUTER JOIN states" +
+                " ON cities.state_id=states.id" +
+                " WHERE states.name='{}'".format(sys.argv[4]))
     query_rows = cur.fetchall()
     print(", ".join([row[0] for row in query_rows]))
     cur.close()
