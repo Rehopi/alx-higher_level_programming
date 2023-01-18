919446c93092fbc94b7962c979525d63fbda446e
rehopi <emekaokechukwu16@gmail.com>
2023-01-17 17:11:36 -0800

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,3 @@
 #!/usr/bin/node
-/* The first argument is the file path
-The second argument is the string to write
-The content of the file must be written in utf-8
-If an error occurred during while writing, print the error object */
-
 const fs = require('fs');
-
-let text = process.argv[3];
-
-fs.writeFile(process.argv[2], text, { encoding: 'utf-8' }, (err) => {
-  // catch error
-  if (err) {
-    console.log(err);
-  }
-});
+fs.writeFileSync(process.argv[2], process.argv[3]);
