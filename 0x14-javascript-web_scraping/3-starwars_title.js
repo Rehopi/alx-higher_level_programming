68e8c7408b98c9179e8aba8f45d30e6c497c2df5
rehopi <emekaokechukwu16@gmail.com>
2023-01-17 17:36:46 -0800

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-let resultDict = {};
-request(url, function (error, response, body) {
-  if (error) {
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id + '/';
+const axios = require('axios').default;
+
+// Make a request for a user with a given ID
+axios.get(url)
+  .then(function (response) {
+    // handle success
+    console.log(response.data.title);
+  })
+  .catch(function (error) {
+    // handle error
     console.log(error);
-  } else {
-    resultDict = JSON.parse(body);
-    console.log(resultDict.title);
-  }
-});
+  })
+  .then(function () {
+    // always executed
+  });
