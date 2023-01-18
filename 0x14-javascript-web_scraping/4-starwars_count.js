3f4e2e9713f2ad5e8d05865f451c09cc51bf82f6
rehopi <emekaokechukwu16@gmail.com>
2023-01-17 18:05:19 -0800

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,25 @@
 #!/usr/bin/node
-const request = require('request');
 const url = process.argv[2];
-let resultDict = {};
-let count = 0;
-let i = 0;
-let j = 0;
-request(url, function (
-  error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    resultDict = JSON.parse(body);
-    for (i = 0; i < resultDict.results.length; i++) {
-      for (j = 0; j < resultDict.results[i].characters.length; j++) {
-	if (resultDict.results[i].characters[j].includes('/18/')) {
-	  count++;
+const axios = require('axios').default;
+
+// Make a request for a user with a given ID
+axios.get(url)
+  .then(function (response) {
+    let count = 0;
+    for (let i = 0; response.data.results[i]; i++) {
+      const info = response.data.results[i].characters;
+      info.forEach(element => {
+	if (element.search('18') !== -1) {
+          count++;
 	}
-      }
+      });
     }
     console.log(count);
-  }
-});
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
