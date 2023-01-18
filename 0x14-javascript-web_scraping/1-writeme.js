24157c167ec544a60b9237115e3861db216432b2
rehopi <emekaokechukwu16@gmail.com>
2023-01-17 17:09:54 -0800

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+/* The first argument is the file path
+The second argument is the string to write
+The content of the file must be written in utf-8
+If an error occurred during while writing, print the error object */
+
 const fs = require('fs');
 
 let text = process.argv[3];
