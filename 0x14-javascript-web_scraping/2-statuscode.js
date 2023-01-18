875c2226ba37d8608f09797b20753346eb7cbd4c
rehopi <emekaokechukwu16@gmail.com>
2023-01-17 17:27:34 -0800

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-const url = process.argv[2];
-const axios = require('axios').default;
-
-// Make a request for a user with a given ID
-axios.get(url)
-  .then(function (response) {
-    // handle success
-    console.log('code:', response.status);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log('code: ' + error.response.status);
-  })
-  .then(function () {
-    // always executed
-  });
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
+});
