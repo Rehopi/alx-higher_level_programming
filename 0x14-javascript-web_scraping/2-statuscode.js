33b12068f3f423feebf2aa6f7b4c12dc6b6a6acd
rehopi <emekaokechukwu16@gmail.com>
2023-01-17 17:18:05 -0800

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 /* a script that display the status code of a GET request;
-The first argument is the URL to request (GET)
-The status code must be printed like this: code: <status code>
-You must use the module request */
+ * The first argument is the URL to request (GET)
+ * The status code must be printed like this: code: <status code>
+ * You must use the module request 
+ */
 
 const request = require('request');
 
