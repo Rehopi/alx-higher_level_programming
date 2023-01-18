7a09d95e2f5c2182d1e7c0bf6539f08d07fb3baa
rehopi <emekaokechukwu16@gmail.com>
2023-01-17 18:48:54 -0800

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
+const axios = require('axios').default;
 const url = process.argv[2];
-let text;
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    text = body;
-    fs.writeFile(process.argv[3], text, 'utf-8', (err) => {
-      if (err) {
-	console.log(err);
-      }
-    });
-  }
-});
+axios.get(url)
+  .then(function (response) {
+    try {
+      fs.writeFileSync(process.argv[3], response.data, { encoding: 'utf-8', flag: 'w+' });
+    } catch (err) {
+      console.log(err);
+    }
+  });
